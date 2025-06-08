d02944e0b31174d7b1c89fc9afcc4d32b70aeb3c
EchoedDestiny428 <zacharyng428@gmail.com>
2025-06-08 10:58:48 +0800

    change to cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
+#include "resource_dir.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
-
-int main ()
-{
+int main () {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
